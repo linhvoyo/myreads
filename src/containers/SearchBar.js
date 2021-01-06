a7baf82f2421279ca96bebe8c87276fb62424fa9
linhvoyo <linhvoyo@ymail.com>
2021-01-06 08:36:00 -0800

    feat: Implement live search
    Change on submit search to on change

--- src/containers/SearchBar.js
+++ src/containers/SearchBar.js
@@ -7,18 +7,16 @@ export default class SearchBar extends React.Component {
   }
 
   searchHandler = (event) => {
-    this.setState({ query: event.target.value })
+    this.setState({ query: event.target.value },
+      () => this.props.onSearch(event, this.state.query));
   }
 
   render() {
-    const { onSearch } = this.props;
-    return <form onSubmit={(event) => onSearch(event, this.state.query)}>
-      <input
+    return <input
         className='search-books'
         type='text'
         value={this.state.query}
         placeholder='Search Books'
-        onChange={this.searchHandler}></input>
-    </form>
+        onChange={this.searchHandler} />
   }
 }
